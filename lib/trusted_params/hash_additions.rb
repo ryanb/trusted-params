525c3f2c093cd1b09d50a321bc754cf7aa2fe692
Ryan Bates <ryan@railscasts.com>
2009-06-01 10:03:18 -0700

    adding some basic trust behavior to all hashes

--- lib/trusted_params/hash_additions.rb
+++ lib/trusted_params/hash_additions.rb
@@ -1,9 +1,25 @@
 module TrustedParams
   module HashAdditions
+    def trust(*attribute_names)
+      if attribute_names.empty?
+        @trusted_attributes = :all
+      else
+        @trusted_attributes = attribute_names
+        attribute_names.each do |attribute_name|
+          self[attribute_name].trust if self[attribute_name].kind_of? Hash
+        end
+      end
+    end
     
+    def trusted?(attribute_name)
+      if defined?(@trusted_attributes)
+        @trusted_attributes == :all || @trusted_attributes.include?(attribute_name)
+      end
+    end
   end
 end
 
+# I would prefer not setting this in all hashes, but it is the easiest solution at the moment.
 class Hash
   include TrustedParams::HashAdditions
 end
--- spec/trusted_params/hash_additions_spec.rb
+++ spec/trusted_params/hash_additions_spec.rb
@@ -1,4 +1,36 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Hash do
+  before(:each) do
+    @hash = { :foo => "bar" }
+  end
+  
+  it "should not consider attributes trusted by default" do
+    @hash.should_not be_trusted(:foo)
+  end
+  
+  it "should trust attributes marked as trusted" do
+    @hash.trust(:foo)
+    @hash.should be_trusted(:foo)
+  end
+  
+  it "should trust all attributes when passing no arguments" do
+    @hash.trust
+    @hash.should be_trusted(:foo)
+    @hash.should be_trusted(:anything)
+  end
+  
+  it "should trust multiple attributes in one call" do
+    @hash.trust(:foo, :hello)
+    @hash.should be_trusted(:foo)
+    @hash.should be_trusted(:hello)
+    @hash.should_not be_trusted(:anything)
+  end
+  
+  it "should inherit trust in nested hashes" do
+    @hash[:child] = { :boing => "bong" }
+    @hash.trust(:child)
+    @hash.should be_trusted(:child)
+    @hash[:child].should be_trusted(:boing)
+  end
 end
